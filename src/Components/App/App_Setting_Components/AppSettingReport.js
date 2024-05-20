700eb92f116c622cd30008dcabab4a072170f1c7
YuJin <103713433+YujinKim164@users.noreply.github.com>
2024-05-21 00:30:18 +0900

    edit report tab UI

--- src/Components/App/App_Setting_Components/AppSettingReport.js
+++ src/Components/App/App_Setting_Components/AppSettingReport.js
@@ -1,20 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "../../../Style/theme";
-import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import i18n from "../../../locales/i18n";
 
 const AppSettingReport = () => {
-  const navigate = useNavigate();
   const { t } = useTranslation();
 
+  const TabData = [
+    { id: 1, button: t("register"), content: "새로운 장소를 등록해주세요." },
+    { id: 2, button: t("edit"), content: "장소 정보를 수정해주세요." },
+  ];
+
+  const [activeTab, setActiveTab] = useState(TabData[0].id);
   return (
     <ThemeProvider theme={theme}>
       <TopAppBar>
         <TopAppBarTxt>{t("feedback")}</TopAppBarTxt>
       </TopAppBar>
+      <FooterBox>
+        <TabBox>
+          {TabData.map((tab) => (
+            <TabButton
+              key={tab.id}
+              data-active={activeTab === tab.id ? "true" : "false"}
+              onClick={() => setActiveTab(tab.id)}
+            >
+              {tab.button}
+            </TabButton>
+          ))}
+        </TabBox>
+        <TabContent>
+          {TabData.find((a) => a.id === activeTab)?.content}
+        </TabContent>
+      </FooterBox>
       <Link to="/">{t("homePage")}</Link>
     </ThemeProvider>
   );
@@ -31,6 +51,33 @@ const TopAppBar = styled.div`
 const TopAppBarTxt = styled.div`
   font-size: 20px;
   color: #ffffff;
+`;
+
+const FooterBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 9.375rem;
+  padding-top: 10px;
+`;
+
+const TabBox = styled.div`
+  display: flex;
+  border-bottom: 1px solid #ccc;
+`;
+
+const TabButton = styled.button`
+  border: none;
+  padding: 10px 20px;
+  background-color: ${({ "data-active": dataActive }) =>
+    dataActive === "true" ? "#f0ece3" : "transparent"};
+  color: ${({ "data-active": dataActive }) =>
+    dataActive === "true" ? "black" : "grey"};
+  cursor: pointer;
+`;
+
+const TabContent = styled.div`
+  padding: 10px;
 `;
 
 export default AppSettingReport;
