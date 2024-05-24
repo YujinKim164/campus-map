ad43cee936cdde97cf77a6098de0f694953dc839
YuJin <103713433+YujinKim164@users.noreply.github.com>
2024-05-24 14:46:48 +0900

    edit report modal

--- src/Components/App/App_Setting_Components/AppSettingReport.js
+++ src/Components/App/App_Setting_Components/AppSettingReport.js
@@ -4,16 +4,24 @@ import { theme } from "../../../Style/theme";
 import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 import i18n from "../../../locales/i18n";
+import Modal from "react-modal";
 
 const AppSettingReport = () => {
   const { t } = useTranslation();
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const TabData = [
     { id: 1, button: t("register"), content: "새로운 장소를 등록해주세요." },
     { id: 2, button: t("edit"), content: "장소 정보를 수정해주세요." },
   ];
-
   const [activeTab, setActiveTab] = useState(TabData[0].id);
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <ThemeProvider theme={theme}>
       <TopAppBar>
@@ -35,6 +43,19 @@ const AppSettingReport = () => {
           {TabData.find((a) => a.id === activeTab)?.content}
         </TabContent>
       </FooterBox>
+      <ReportButton onClick={openModal}>{t("report")}</ReportButton>
+      {/* 모달 창 */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Language Selection"
+        style={customStyles}
+      >
+        <ModalContent>
+          <ModalDetail>{t("reportModal")}</ModalDetail>
+          <ModalCloseButton onClick={closeModal}>{t("ok")}</ModalCloseButton>
+        </ModalContent>
+      </Modal>
       <Link to="/">{t("homePage")}</Link>
     </ThemeProvider>
   );
@@ -78,6 +99,54 @@ const TabButton = styled.button`
 
 const TabContent = styled.div`
   padding: 10px;
+`;
+
+const ReportButton = styled.div`
+  text-align: right;
+  padding: 16px 30px;
+  font-size: 16px;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.black_90};
+`;
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+    width: "293px",
+    borderRadius: "10px",
+    textAlign: "center",
+    padding: "0",
+  },
+  overlay: {
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+  },
+};
+
+const ModalContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  text-align: left;
+`;
+
+const ModalDetail = styled.div`
+  font-size: 18px;
+  font-weight: 500;
+  padding: 30px 24px 0 24px;
+  color: ${(props) => props.theme.colors.black_90};
+`;
+
+const ModalCloseButton = styled.div`
+  text-align: right;
+  margin-top: 29px;
+  padding: 0 30px 20px 0;
+  font-size: 16px;
+  font-weight: 500;
+  color: ${(props) => props.theme.colors.Primary_blue};
 `;
 
 export default AppSettingReport;
